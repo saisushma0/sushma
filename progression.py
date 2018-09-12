613d065915d3fc3deb745e95be7c5fcc70215c8d
saisushma0 <42973473+saisushma0@users.noreply.github.com>
2018-09-12 19:58:50 +0530

    Create progression.py

---
+++ progression.py
@@ -0,0 +1,9 @@
+
+  
+def Nth_of_AP(a, d, N) : 
+    return (a + (N - 1) * d) 
+a = 2  # starting number 
+d = 1  # Common difference 
+N = 5  # N th term to be find 
+print( "The ", N ,"th term of the series is : ", 
+       Nth_of_AP(a, d, N)) 
